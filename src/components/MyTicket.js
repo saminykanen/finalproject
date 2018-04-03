977bbd1b9f9b6d0f8ca664f0c79643027e665695
anvais <antti.j.valtonen@alumni.aalto.fi>
2018-04-03 16:20:30 +0300

    Muutettu nimet tietokantojen mukaisiksi.

--- src/components/MyTicket.js
+++ src/components/MyTicket.js
@@ -3,25 +3,14 @@ import TicketForm from "./TicketForm";
 import {ticketJson} from './TicketList';
 
 class MyTicket extends Component {
-    state = {data:[]};
-    componentDidMount() {
-        fetch('api/tickets')
-            .then(function(response) {return response.json();})
-            .then(function(json) {
-                this.setState({data: json});
-            }.bind(this));
-        console.log("data tuli");
-    }
 
     constructor(props) {
         super(props);
-
-/*        this.state= {tickets: data};*/
     }
 
     addNewTicket = (e) => {
         this.state.data.push(e);
-    }
+    };
 
     render(){
         return(
--- src/components/TicketForm.js
+++ src/components/TicketForm.js
@@ -5,12 +5,12 @@ class TicketForm extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            tickettitle: '',
-            ticketdescription: '',
-            ticketowner: '', //tuleeko tähän se kirjautunut?
-            ticketstatus: 'queue',
+            ticketTitle: '',
+            ticketDescription: '',
+            userName: '', //tuleeko tähän se kirjautunut?
+            ticketStatus: 'queue',
             timestamp: '', //tähän joku localdate now?
-            courseid: '', //tämäkin automatic?
+            courseId: '', //tämäkin automatic?
             location: ''
         }
         this.handleInputChange = this.handleInputChange.bind(this);
@@ -29,12 +29,12 @@ class TicketForm extends Component {
         e.preventDefault();
         this.props.addNew(this.state);
         this.setState({
-                tickettitle: '',
-                ticketdescription: '',
-                ticketowner: '', //tuleeko tähän se kirjautunut?
-                ticketstatus: 'queue',
+                ticketTitle: '',
+                ticketDescription: '',
+                userName: '', //tuleeko tähän se kirjautunut?
+                ticketStatus: 'queue',
                 timestamp: '', //tähän joku localdate now?
-                courseid: '', //tämäkin automatic?
+                courseId: '', //tämäkin automatic?
                 location: ''
             });
     }
@@ -45,9 +45,9 @@ class TicketForm extends Component {
             <div>
                 <h1>Add new ticket</h1>
                 <form onSubmit={this.handleSubmitting}>
-                    <input type="text" value={this.state.ticketowner} onChange={this.handleInputChange} placeholder={"Owner"}/> <br/>
-                    <input type="text" value={this.state.tickettitle} onChange={this.handleInputChange} placeholder={"Title"}/><br/>
-                    <input type="text" value={this.state.ticketdescription} onChange={this.handleInputChange} placeholder={"Description"}/><br/>
+                    {/*<input type="text" value={this.state.userName} onChange={this.handleInputChange} placeholder={"Owner"}/> <br/>*/}
+                    <input type="text" value={this.state.ticketTitle} onChange={this.handleInputChange} placeholder={"Title"}/><br/>
+                    <input type="text" value={this.state.ticketDescription} onChange={this.handleInputChange} placeholder={"Description"}/><br/>
                     <input type="text" value={this.state.location} onChange={this.handleInputChange} placeholder={"Location"}/><br/>
                     <input type="submit" value="Add ticket"/>
                 </form>
