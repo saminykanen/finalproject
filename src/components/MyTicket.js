e667b6b6a9965ef920a0d748c911d1db36394a85
satuylikoski <satu.ylikoski@hotmail.com>
2018-04-03 14:45:43 +0300

    ensimmäinen versio formista

--- src/components/MyTicket.js
+++ src/components/MyTicket.js
@@ -1,12 +1,24 @@
 import React, {Component} from 'react';
 import TicketForm from "./TicketForm";
+import {ticketJson} from './TicketList';
 
-class MyTicket extends Component{
+class MyTicket extends Component {
+    state = {};
+
+    constructor(props) {
+        super(props);
+        this.state= {tickets: ticketJson};
+    }
+
+    addNewTicket = (e) => {
+        this.state.tickets.push(e);
+    }
+
     render(){
         return(
             <div className="container">
                 <span>MyTicket</span>
-                <TicketForm/>
+                <TicketForm addNew = {this.addNewTicket}/>
             </div>
         )
     }
--- src/components/TicketForm.js
+++ src/components/TicketForm.js
@@ -1,10 +1,56 @@
 import React, {Component} from 'react';
+import {ticketJson} from "./TicketList";
 
-class TicketForm extends Component{
+class TicketForm extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            tickettitle: '',
+            ticketdescription: '',
+            ticketowner: '', //tuleeko tähän se kirjautunut?
+            ticketstatus: 'queue',
+            timestamp: '', //tähän joku localdate now?
+            courseid: '', //tämäkin automatic?
+            location: ''
+        }
+        this.handleInputChange = this.handleInputChange.bind(this);
+        this.handleSubmitting = this.handleSubmitting.bind(this);
+    }
+
+    handleInputChange(e) {
+        const target = e.target;
+        const value = target.value;
+        const name = target.name;
+
+        this.setState({[name] : value})
+    }
+
+    handleSubmitting(e) {
+        e.preventDefault();
+        this.props.addNew(this.state);
+        this.setState({
+                tickettitle: '',
+                ticketdescription: '',
+                ticketowner: '', //tuleeko tähän se kirjautunut?
+                ticketstatus: 'queue',
+                timestamp: '', //tähän joku localdate now?
+                courseid: '', //tämäkin automatic?
+                location: ''
+            });
+    }
+
+
     render(){
         return(
-            <div className="container">
-                <span>TicketForm</span>
+            <div>
+                <h1>Add new ticket</h1>
+                <form onSubmit={this.handleSubmitting}>
+                    <input type="text" value={this.state.ticketowner} onChange={this.handleInputChange} placeholder={"Owner"}/> <br/>
+                    <input type="text" value={this.state.tickettitle} onChange={this.handleInputChange} placeholder={"Title"}/><br/>
+                    <input type="text" value={this.state.ticketdescription} onChange={this.handleInputChange} placeholder={"Description"}/><br/>
+                    <input type="text" value={this.state.location} onChange={this.handleInputChange} placeholder={"Location"}/><br/>
+                    <input type="submit" value="Add ticket"/>
+                </form>
             </div>
         )
     }
