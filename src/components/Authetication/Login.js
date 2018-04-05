7baf10afcbeab8cc2c84beea31b1a6ba8c452144
Veli-Pekka Nurmi <nurmi.vp@gmail.com>
2018-04-05 10:47:29 +0300

    valmis yhdistettäväksi Tonin kanssa

--- src/components/Authetication/Login.js
+++ src/components/Authetication/Login.js
@@ -73,7 +73,7 @@ class Login extends Component {
                 }}/>
 
                 <button onClick={() => {
-                this.autWithFacebook()
+                    this.autWithFacebook()
                 }}>Login with Facebook
                 </button>
 
