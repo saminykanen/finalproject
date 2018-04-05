0bdd4769301b044c0bc7feeb7c445ade930ad2db
saminykanen <nsami89@gmail.com>
2018-04-05 15:39:22 +0300

    muutettu th/td -> div/span

--- src/components/Ticket.js
+++ src/components/Ticket.js
@@ -9,31 +9,40 @@ class Ticket extends Component {
     render() {
         return (
             <div className="container">
-                <Panel defaultCollapsed className={this.props.ticket.ticketStatus.startsWith('a') ? "panel panel-success" : "defaultticket"}>
-                    <Panel.Heading >
+                <Panel defaultcollapsed="true"
+                       className={this.props.ticket.ticketStatus.startsWith('a') ? "panel panel-success" : "defaultticket"}>
+                    <Panel.Heading>
                         <Panel.Title toggle className="text-left">
-                            <th>{this.props.ticket.ticketTitle}</th>
-                            <th style={{float: 'right'}}>{this.props.ticket.timestamp}</th>
-                            <img src={expandbutton} width="45" height="12" className="center-block"/>
-
-
+                            <div>
+                                <span>{this.props.ticket.ticketTitle}</span>
+                                <span style={{float: 'right'}}>{this.props.ticket.timestamp}</span>
+                                <img src={expandbutton} width="45" height="12" className="center-block"/>
+                            </div>
                         </Panel.Title>
                     </Panel.Heading>
                     <Panel.Collapse>
                         <Panel.Body className="text-left">
-                            <tr>
-                                <td>{this.props.ticket.location}</td>
-                            </tr>
-                            <tr>
-                                <td>{this.props.ticket.timestamp}</td>
-                            </tr>
-                            <tr>
-                                <td>{this.props.ticket.ticketDescription}</td>
-                            </tr>
-                            <tr>
-                                <td><button className="btn btn-danger btn-md" onClick={this.handleChangeToPassive.bind(this)}>DELETE</button></td>
-                                <td><button className="btn btn-warning btn-md float-right" onClick={this.handleChangeToActive.bind(this)}>ACTIVATE</button></td>
-                            </tr>
+                            <div>
+                                <span>{this.props.ticket.location}</span>
+                            </div>
+                            <div>
+                                <span>{this.props.ticket.timestamp}</span>
+                            </div>
+                            <div>
+                                <span>{this.props.ticket.ticketDescription}</span>
+                            </div>
+                            <div>
+                                <span>
+                                    <button className="btn btn-danger btn-md"
+                                            onClick={this.handleChangeToPassive.bind(this)}>DELETE
+                                    </button>
+                                </span>
+                                <span>
+                                    <button className="btn btn-warning btn-md float-right"
+                                            onClick={this.handleChangeToActive.bind(this)}>ACTIVATE
+                                    </button>
+                                </span>
+                            </div>
                         </Panel.Body>
                     </Panel.Collapse>
                 </Panel>
@@ -46,19 +55,20 @@ class Ticket extends Component {
         const API = 'api//tickets/setpassive/';
         const TID = this.props.ticket.ticketId;
 
-        fetch(API+TID,{
+        fetch(API + TID, {
             method: 'PUT'
         })
             .then(function () {
                 this.props.reFetchList();
             }.bind(this));
     }
-        handleChangeToActive(e) {
+
+    handleChangeToActive(e) {
         e.preventDefault();
         const API = 'api//tickets/setactive/';
         const TID = this.props.ticket.ticketId;
 
-        fetch(API+TID,{
+        fetch(API + TID, {
             method: 'PUT'
         })
             .then(function () {
