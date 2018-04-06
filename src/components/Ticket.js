c9768ce1a1f9f866196f726f04c8547cbd731724
saminykanen <nsami89@gmail.com>
2018-04-06 10:04:29 +0300

    paneelien auto-collapse leikkiminen kesken

--- src/components/Ticket.js
+++ src/components/Ticket.js
@@ -2,51 +2,74 @@ import React, {Component} from 'react';
 import {Panel} from 'react-bootstrap';
 import './Ticket.css';
 import expandbutton from '../images/expandbutton.png';
+import PanelGroup from "react-bootstrap/es/PanelGroup";
 
 
 class Ticket extends Component {
+    constructor(props) {
+        super(props);
+
+        this.handleSelect = this.handleSelect.bind(this);
+
+        this.state = {
+            activeKey: '1'
+        };
+    }
+
+    handleSelect(activeKey) {
+        this.setState({activeKey});
+    }
 
     render() {
         return (
             <div className="container">
-                <Panel defaultcollapsed="true"
-                       className={this.props.ticket.ticketStatus.startsWith('a') ? "panel panel-success" : "defaultticket"}>
-                    <Panel.Heading>
-                        <Panel.Title toggle className="text-left">
-                            <div>
-                                <span><b>{this.props.ticket.ticketTitle}</b></span>
-                                <span style={{float: 'right'}}>{this.props.ticket.timestamp}</span>
-                                <img src={expandbutton} width="45" height="12" className="center-block" alt="expandticket"/>
-                            </div>
-                        </Panel.Title>
-                    </Panel.Heading>
-                    <Panel.Collapse>
-                        <Panel.Body className="text-left">
-                            <div className="row">
-                                <span className="col-md-1"><b>Location:</b></span><span className="col-md-11">{this.props.ticket.location}</span>
-                            </div>
-                            <div className="row">
-                                <span className="col-md-1"><b>Date&Time:</b></span><span className="col-md-11">{this.props.ticket.timestamp}</span>
-                            </div>
-                            <div className="row">
-                                <span className="col-md-1"><b>Description:</b></span><span className="col-md-11">{this.props.ticket.ticketDescription}</span>
-                            </div>
-                            <br/>
-                            <div>
+                <PanelGroup accordion
+                            id="accordion-controlled-example"
+                            activeKey={this.state.activeKey}
+                            onSelect={this.handleSelect}>
+                    <Panel eventKey="1" defaultcollapsed="true"
+                           className={this.props.ticket.ticketStatus.startsWith('a') ? "panel panel-success" : "defaultticket"}>
+                        <Panel.Heading>
+                            <Panel.Title toggle className="text-left">
+                                <div>
+                                    <span><b>{this.props.ticket.ticketTitle}</b></span>
+                                    <span style={{float: 'right'}}>{this.props.ticket.timestamp}</span>
+                                    <img src={expandbutton} width="45" height="12" className="center-block"
+                                         alt="expandticket"/>
+                                </div>
+                            </Panel.Title>
+                        </Panel.Heading>
+                        <Panel.Collapse>
+                            <Panel.Body collapsible className="text-left">
+                                <div className="row">
+                                    <span className="col-md-1"><b>Location:</b></span><span
+                                    className="col-md-11">{this.props.ticket.location}</span>
+                                </div>
+                                <div className="row">
+                                    <span className="col-md-1"><b>Date&Time:</b></span><span
+                                    className="col-md-11">{this.props.ticket.timestamp}</span>
+                                </div>
+                                <div className="row">
+                                    <span className="col-md-1"><b>Description:</b></span><span
+                                    className="col-md-11">{this.props.ticket.ticketDescription}</span>
+                                </div>
+                                <br/>
+                                <div>
                                 <span>
                                     <button className="btn btn-danger btn-md" style={{marginRight: '10px'}}
                                             onClick={this.handleChangeToPassive.bind(this)}>DELETE
                                     </button>
                                 </span>
-                                <span>
+                                    <span>
                                     <button className="btn btn-warning btn-md float-right"
                                             onClick={this.handleChangeToActive.bind(this)}>ACTIVATE
                                     </button>
                                 </span>
-                            </div>
-                        </Panel.Body>
-                    </Panel.Collapse>
-                </Panel>
+                                </div>
+                            </Panel.Body>
+                        </Panel.Collapse>
+                    </Panel>
+                </PanelGroup>
             </div>
         )
     }
