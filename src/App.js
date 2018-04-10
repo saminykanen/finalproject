add6c48d3e1d05b97ad97562d53c0a6a26989b30
saminykanen <nsami89@gmail.com>
2018-04-10 11:57:52 +0300

    lisätty ohjekuva jos käyttäjä ei ole millään kurssilla

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import Login from "./components/Authetication/Login";
 import {app} from "./components/Authetication/base";
 import {BrowserRouter as Router, Switch, Route, Link} from 'react-router-dom';
 import { Navigation } from './components/Navigation';
-
+import nocourseimg from './images/nocourseimg.png';
 
 class App extends Component {
 
@@ -163,10 +163,13 @@ class App extends Component {
     showStuffBasedOnLoginAndCourseStatus(){
         if (this.state.courses.length === 0 && this.state.authenticated === true){
             return(
+                <div>
                 <form className="default" onSubmit={this.fetchCourseTickets}>
                     <input type="text" name="kurssiId" placeholder="Kurssitunnus"/>
                     <button>Find course</button>
                 </form>
+                    <img className="center-block" src={nocourseimg}/>
+                </div>
             )
         }else if(this.state.authenticated === true){
             return(
