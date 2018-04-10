c05f862850341a29c47fb6eb278e91da7d6d0451
satuylikoski <satu.ylikoski@hotmail.com>
2018-04-10 21:35:31 +0300

    lisätty tollane

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -2,19 +2,27 @@ import React, {Component} from 'react';
 import { Navigation } from './Navigation';
 import Title from './Title';
 import "./Profile.css";
+import {app} from "../components/Authetication/base";
 
 class Profile extends Component {
 
     render() {
 
+        /*var currentUser = app.auth().currentUser;
+        console.log("currentUser " + currentUser);*/
+
+        console.log(this.props.authenticated);
+
         return (
             <div className="default">
-                <Navigation />
-                <Title />
+                <Navigation/>
+                <Title/>
                 <h3>Profile information</h3>
             </div>
         );
+        /*}*/
     }
+
 }
 
 export default Profile;
