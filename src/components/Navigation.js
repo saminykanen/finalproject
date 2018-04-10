eb3c32bd68185ce3649fda0167f988ffad267e42
satuylikoski <satu.ylikoski@hotmail.com>
2018-04-10 12:16:45 +0300

    lisätty logo

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './Navigation.css';
 import { Navbar } from 'react-bootstrap';
+import academy_logo from '../images/academy_logo.png';
 
 
 
@@ -8,15 +9,22 @@ export const Navigation = () => (
 
 <Navbar className="navbar">
     <Navbar.Header>
+        <Navbar.Brand><img src={academy_logo}/>
+        </Navbar.Brand>
     <Navbar.Toggle />
     </Navbar.Header>
-    <Navbar.Collapse>
+    <Navbar.Collapse >
         <Navbar.Text pullRight>
             <Navbar.Link className="glyphicon glyphicon-log-out" href="/logout"> Logout</Navbar.Link>
         </Navbar.Text>
     <Navbar.Text pullRight>
         <Navbar.Link className="glyphicon glyphicon-user" href="#"> Profile</Navbar.Link>
     </Navbar.Text>
+        <Navbar.Text>
+        <Navbar.Form>
+        </Navbar.Form>
+        </Navbar.Text>
+
     </Navbar.Collapse>
 </Navbar>
 );
